9fd6a533e2de04718d0dd1ee78ceb151ca1968cd
vagrant <Dcameroncheng@gmail.com>
2020-05-04 22:38:37 +0000

    align text on confirmation page

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router  = express.Router();
+const pg = require('pg');
 
 module.exports = () => {
   router.get("/", (req, res, next) => {
     res.render('pages/confirmation');
-
-
-
-
   })
+
+  router.post
 
   return router;
 };
