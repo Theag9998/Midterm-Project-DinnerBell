a89dbe24f4f321d324edab67b10ff4448042d71c
vagrant <Dcameroncheng@gmail.com>
2020-05-04 18:19:30 +0000

    remove head to include head partial

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -4,7 +4,7 @@ const router  = express.Router();
 module.exports = () => {
   router.get("/", (req, res, next) => {
     res.render('pages/confirmation');
-    
+
 
 
 
@@ -12,5 +12,3 @@ module.exports = () => {
 
   return router;
 };
-
-localStorage.clear();
