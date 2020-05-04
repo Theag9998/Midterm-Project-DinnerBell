3cea0d23a1e4013d135a3ccd49210dfcff7dc0ae
vagrant <Dcameroncheng@gmail.com>
2020-05-04 22:56:24 +0000

    working master

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router  = express.Router();
-const pg = require('pg');
 
-module.exports = () => {
+
+module.exports = (db) => {
   router.get("/", (req, res, next) => {
     res.render('pages/confirmation');
   })
 
-  router.post
+  router.get("/", (req, res, next) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  })
 
   return router;
 };
