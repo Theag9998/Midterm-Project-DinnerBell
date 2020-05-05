c85808880335dddd336a03ae230f1f3dbefbbbda
vagrant <Dcameroncheng@gmail.com>
2020-05-05 21:47:22 +0000

    fix merge conflict

--- routes/checkout.js
+++ routes/checkout.js
@@ -18,48 +18,6 @@ module.exports = (db) => {
     res.render("pages/checkout")
   });
 
-<<<<<<< HEAD
-  // router.post("/", (req, res) => {
-  //   // SMS to Customer
-  //   client.messages.create({
-  //   to: process.env.twilio_account_sid, 
-  //   from: '+17739455987',
-  //   body: 'Order received. Your order will be ready in 30 minutes. Please pick up at Papa Joe\'s Restaurant at 7:30pm.'
-  //   })
-  //   .then((message) => console.log(message.sid));
-
-  //   //SMS to Restaurant
-  //   client.messages.create({
-  //   to: process.env.twilio_account_sid, 
-  //   from: '+17739455987',
-  //   body: 'New order received!'
-  // })
-  //   .then((message) => console.log(message.sid));
-//})
-
-  router.put("/:id", (req, res) => {
-    // UPDATE database
-    // redirect to confirmation page.
-      return db.query (`
-        UPDATE orders
-        SET order_date_time = NOW()
-        WHERE orders.id = $1
-        RETURNING id;
-      `, [req.params.id])
-        .then(data => {
-          const queries = [];
-          let values = [];
-          let counter = 1;
-          for (const food of cart.foods ) {
-            queries.push(`UPDATE order_foods SET food_id = $${counter}, order_id = $${counter + 1}, quantity = $${counter + 2} WHERE order_foods.id = $${counter + 3};`);
-            values = values.concat([ cart.foods.food_id, data.id, cart.foods.quantity, cart.foods.id]);
-            counter += 4;
-          }
-          const queryString = queries.join('\n');
-          return db.query(queryString, values)
-            .then(data => res.redirect('/confirmation'));
-        });
-=======
   router.post("/", (req, res) => {
     const customerId = 1;
     const orderId = null;
@@ -104,7 +62,6 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
->>>>>>> a399121c6bdd5e03c4d9b5090914c977df59c3ce
   });
   return router;
 };
