d84fe0e2f4b6eaf9e470d21984a76120d293d441
vagrant <Dcameroncheng@gmail.com>
2020-05-05 16:16:59 +0000

    new twilio number

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const router  = express.Router();
-const accountSid = 'AC374bb6cd27fbae9a937c7526526c433e';
-const authToken = '965169039fd80dbc5f4ef68bbaab44d5';
 
-//const client = require ('twilio') (accountSid, authToken);
 
-//SMS to Customer
-// client.messages.create({
-//   to: +7783192221,
-//   from: '+15153163985',
-//   body: 'Order received. Your order will be ready in 30 minutes. Please pick up at Papa Joe\'s Restaurant at 7:30pm.'
-// })
-// .then((message) => console.log(message.sid));
+// const client = require ('twilio') (accountSid, authToken);
 
-// //SMS to Restaurant
-// client.messages.create({
-//   to: +17783192221,
-//   from: '+15153163985',
-//   body: 'New order received!'
-// })
-// .then((message) => console.log(message.sid));
+
 
 module.exports = (db) => {
 
@@ -27,6 +12,23 @@ module.exports = (db) => {
     res.render("pages/checkout")
   });
 
+  // router.post("/", (req, res) => {
+  //   // SMS to Customer
+  //   client.messages.create({
+  //   to: process.env.twilio_account_sid, 
+  //   from: '+17739455987',
+  //   body: 'Order received. Your order will be ready in 30 minutes. Please pick up at Papa Joe\'s Restaurant at 7:30pm.'
+  //   })
+  //   .then((message) => console.log(message.sid));
+
+  //   //SMS to Restaurant
+  //   client.messages.create({
+  //   to: process.env.twilio_account_sid, 
+  //   from: '+17739455987',
+  //   body: 'New order received!'
+  // })
+  //   .then((message) => console.log(message.sid));
+})
 
 
 
