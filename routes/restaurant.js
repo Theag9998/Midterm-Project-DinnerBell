b912cbfccba9f222aa270bdcaa7297bd2c7c171a
cameron <cameroncheng@gmail.com>
2020-05-06 20:47:34 +0000

    start styling restaurant page

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (db) => {
-
-  router.get("/", (req, res) => {
-    res.render("index");
-  });
-
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  router.get('/', (req, res, next) => {
+    return db.orders.all(1)
+    .then(data => {
+      let orders = {
+        completed: data.filter(o => o.complete),
+        pending: data.filter(o => !o.complete)
+      }
+      res.render('pages/restaurant', {orders})
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
   return router;
 };
