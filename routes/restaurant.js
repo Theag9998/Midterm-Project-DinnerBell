199be3aaea0aac1cae7130c82209b61750372aea
Amy Bartels <akunrau@gmail.com>
2020-05-06 17:30:52 +0000

    Copy my-orders query into restaurant.js

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const router  = express.Router();
+// const express = require('express');
+// const router  = express.Router();
 
-module.exports = (db) => {
- 
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-  return router;
-};
+// module.exports = (db) => {
+//   router.get("/", (req, res, next) => {
+
+
+
+//     return db.orders.all(1)
+//     .then(data => {
+//       let orders = {
+//         completed: data.filter(o => o.complete),
+//         pending: data.filter(o => !o.complete)
+//       }
+//       console.log('COMPLETE:',orders.completed)
+//       console.log('PENDING:', orders.pending)
+//       res.render("pages/my-orders", {orders})
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+
+//   })
+
+
+
+//   return router;
+// };
