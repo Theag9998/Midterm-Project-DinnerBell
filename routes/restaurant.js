9bd3c04d66ad818e233f6f41a342f52c439c1d4a
cameron <cameroncheng@gmail.com>
2020-05-07 03:07:49 +0000

    update variable for minute

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -22,10 +22,10 @@ module.exports = (db) => {
   //Route to update time 
   router.post('/:id/confirm', (req, res, next) => {
     const orderId = req.params.id;
-    const estimatedTime = req.body.estimatedTime 
-    db.orders.confirm(orderId, estimatedTime);
+    const minute = req.body.estimatedTime 
+    db.orders.confirm(orderId, minute);
 
-    const estimatedTime = new Date(new Date().getTime() + req.body.estimatedTime*60*1000)
+    const estimatedTime = new Date(new Date().getTime() + minute*60*1000)
     // sms.sendMessage(process.env.PHONE, `${estimatedTime.getHours()}:${estimatedTime.getMinutes()}`)
     })
     
