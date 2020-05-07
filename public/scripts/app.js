088c331dddf203aac555c079b1651cee0f792470
Sean Sullivan <dev@seanmedia.ca>
2020-05-07 01:53:15 +0000

    Added jquery function to retrieve pick up time

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,6 +15,8 @@ $(document).ready(function() {
     $.post('/checkout', reqData);
   });
 });
+
+
 //In-Page JQuery to assign food_id to hidden form field: $('add-to-cart').click( (event)=> {
   // $('#something').val(menu_item_id);
   // });
