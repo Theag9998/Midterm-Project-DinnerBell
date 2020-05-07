dea4176a2b391ef4a1730c9ad02191e3af74d367
Amy Bartels <akunrau@gmail.com>
2020-05-07 23:39:01 +0000

    completed my order page with drop down details view

--- public/scripts/my-orders.js
+++ public/scripts/my-orders.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
 
-
   $('.my-orders-button').click(function(event) {
     $(this).closest('.order-row').children('.food-row').slideToggle(100)
 
     console.log(event.target)
   })
-
-  // $('.my-orders-button').click(function(event) {
-  //   $(event.target).parent().parent().next().slideToggle(200);
-  //   console.log(event.target)
-  // })
 
 })
 
