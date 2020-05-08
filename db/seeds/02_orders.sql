e3429c9702832fe75bc1af932b4392b08f8a1cbb
Amy Bartels <akunrau@gmail.com>
2020-05-08 00:55:40 +0000

    edit one seed

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -1,4 +1,4 @@
 INSERT INTO orders (customer_id, order_date_time, pick_up_date_time, complete)
 VALUES (1, '2020-04-30 19:00', '2020-04-30 19:35', true),
 (1, '2020-05-02 17:00', '2020-05-02 17:24', true),
-(1, '2020-05-05 17:00', '2020-05-10 17:24', true);
+(1, '2020-05-05 17:00', '2020-05-05 17:24', true);
